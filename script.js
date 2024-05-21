157cde6dc4f1031caf71259c4d1dcf59ad9dbb1c
Soumya Ranjan Mishra <118365110+smranjanmishra@users.noreply.github.com>
2024-05-21 12:59:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function firstWord(s) {
-  // your code here
-}
+  // Trim leading and trailing whitespace
+  s = s.trim();
 
+  // Find the index of the first space
+  const firstSpaceIndex = s.indexOf(' ');
+
+  // If there is no space, return the entire string
+  if (firstSpaceIndex === -1) {
+    return s;
+  }
+
+  // Return the substring from the beginning to the first space
+  return s.substring(0, firstSpaceIndex);
+}
 // Do not change the code below
 
 const s = prompt("Enter String:");
